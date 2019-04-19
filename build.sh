84a945a1a59074a1b8ace7716becf8fb7a92faa5
RoccoDev <roccodev.business@gmail.com>
2019-04-19 22:14:43 +0200

    Buildscript now builds binaries

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ cd ..
 mkdir server/mc_server_impl/lib
 cd rust-examples/target/release/
 mv server.dll.lib server.lib
+mv *.dll ../../../server/mc_server_impl/lib
 mv *.lib ../../../server/mc_server_impl/lib
 mv *.dylib ../../../server/mc_server_impl/lib
 mv *.so ../../../server/mc_server_impl/lib
@@ -20,5 +21,15 @@ cd ../../../
 # Move the main executable
 cd server/mc_server_impl
 cargo build --release
+cd ../../
 
+mkdir output
+mv server/mc_server_impl/target/release/bbcraft.exe output # Windows
+mv server/mc_server_impl/target/release/bbcraft output # MacOS/Linux
+mv server/mc_server_impl/lib/* output
+cp LICENSE output
+
+cd output
+tar czf ../bbCraft.tar.gz *
 cd ..
+rm -rf output
