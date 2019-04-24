7e1a9f438baa4f2ea3704c46edc275e1dc28b845
RoccoDev <roccodev.business@gmail.com>
2019-04-24 20:26:35 +0200

    Improved CLI
    * Use loop instead of recursion
    * Match-and-get when the command isn't found

--- server/mc_server_impl/src/cli/mod.rs
+++ server/mc_server_impl/src/cli/mod.rs
@@ -8,22 +8,23 @@ use std::net::TcpListener;
 use std::sync::Arc;
 
 pub fn accept_user_input(listener: &TcpListener) {
-    print!("> ");
-    // stdout only flushes on newlines,
-    // and the above 'print' macro does not include one.
-    io::stdout().flush();
+    loop {
+        print!("> ");
+        // stdout only flushes on newlines,
+        // and the above 'print' macro does not include one.
+        io::stdout().flush();
 
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf);
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf);
 
-    match buf.trim() {
-        "stop" => {
-            crate::net::stop(listener);
-        },
-        _ => {
-            println!("Command not found.");
+        match buf.trim() {
+            "stop" => {
+                crate::net::stop(listener);
+            },
+            (input) => {
+                println!("{}: command not found.", input);
+            }
         }
-    }
 
-    accept_user_input(listener);
+    }
 }
